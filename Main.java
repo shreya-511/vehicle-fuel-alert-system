4916811d40f8906949b3184c654895c3eb20a80a
Shreya <shreya@zeliot.in>
2026-02-23 11:28:33 +0530

    Initial commit: Fuel Alert System

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Computer{
+    public static int add(int a,int b){
+        int result =a+b;
+        return result;
+
+    }
+}
+    public static void Main(String[] args){
+        Computer.add(1,2);
+        Computer.add(4,6);
+    }
